94383f504c2358a67dea313c566a328e1c3dc1e5
Ruslan Mironenkov <Ruslan Mironenkov>
2016-11-12 19:03:28 +0200

    push index

--- scripts/common.js
+++ scripts/common.js
@@ -2,5 +2,11 @@
  * Created by GloryManU on 04.07.2016.
  */
 (function(){
-	
+	$('.tech-description-slider').lightSlider({
+		item: 1,
+		speed: 1000,
+		pager: false,
+		loop: true,
+		slideMargin: 0
+	});
 })();
